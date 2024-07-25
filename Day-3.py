cde5ed58dc69235b0dab2f7fe69dcd2ded9200da
Simran_Kodwani <kodwanisimran03@gmail.com>
2024-07-25 19:57:19 +0530

    solution

---
+++ Day-3.py
@@ -0,0 +1,12 @@
+# ****************Conditional statement***********
+# if __name__ == '__main__':
+#     N = int(input().strip())
+# if N%2!=0:
+#     print("Weird")
+
+# elif N%2==0:
+#     if 2<N<5 or N>20:
+#         print("Not Weird")
+#     else:
+#         print("Weird")
+    
